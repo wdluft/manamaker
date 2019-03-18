96e6d1fb9d9b052f93d95d8e2cb78e6575e57f04
Will Luft <william.d.luft@gmail.com>
2019-03-18 16:43:04 -0400

    make links open in new tab

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,18 +5,18 @@ export default function Footer() {
     <footer>
       <ul>
         <li>
-          <a href="https://twitter.com/iamwilldl">
+          <a href="https://twitter.com/iamwilldl" target="_blank">
             <i className="fab fa-twitter" />
           </a>
         </li>
         <li>
-          <a href="https://willluft.com">
+          <a href="https://willluft.com" target="_blank">
             <i className="fas fa-laptop-code" />
             Will Luft
           </a>
         </li>
         <li>
-          <a href="https://github.com/wdluft">
+          <a href="https://github.com/wdluft" target="_blank">
             <i className="fab fa-github" />
           </a>
         </li>
